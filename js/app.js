89ccb2e767f54c152dcf092238ecc1087311d398
Nojotel <nojotel@yandex.ru>
2023-10-05 20:24:58 +0300

    fix
    button, get

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const textarea = document.querySelector(".textarea");
 const padding = textarea.offsetHeight - textarea.clientHeight;
 textarea.oninput = (e) => {
-  textarea.style.height = "auto";
+  textarea.style.height = "40px";
   textarea.style.height = textarea.scrollHeight + padding + "px";
 };
 // Вывод сообщения пользователя
@@ -19,6 +19,41 @@ textarea.addEventListener("keydown", function (e) {
     }
     textarea.value = "";
   }
+  if (textarea.value != "") {
+    send.classList.remove("hidden");
+  } else {
+    send.classList.add("hidden");
+  }
+});
+
+//По кнопке
+const send = document.querySelector(".send");
+send.addEventListener("click", function () {
+  answer.textContent = textarea.value;
+  textarea.value = "";
+  send.classList.add("hidden");
+  textarea.style.height = "40px";
+  click = 1;
+  if (click == 0) {
+    voice.classList.add("active");
+    answer.textContent = "Незнайка, а почему все переверну...";
+    answer.classList.add("click");
+    svg.classList.remove("hidden");
+    textarea.classList.add("hidden");
+  } else if (click == 1) {
+    answer.textContent = "Это, наверно, не Луна перевернулась, а мы сами перевернулись.";
+    svg.classList.add("hidden");
+    voice.classList.remove("active");
+    voice.classList.add("active_success");
+    textarea.classList.add("hidden");
+  } else if (click == 2) {
+    voice.classList.remove("active_success");
+    answer.textContent = "говорите";
+    answer.classList.remove("click");
+    textarea.classList.remove("hidden");
+    click = -1;
+  }
+  click++;
 });
 
 //Micro
@@ -26,6 +61,13 @@ const voice = document.querySelector(".voice");
 const svg = document.querySelector(".svg");
 let click = 0;
 voice.addEventListener("click", function () {
+  if (answer.textContent == "говорите") {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      console.log(stream.active);
+    });
+  }
+  send.classList.add("hidden");
+
   if (click == 0) {
     voice.classList.add("active");
     answer.textContent = "Незнайка, а почему все переверну...";
