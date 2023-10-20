176a0a396cfd29006126d0f063791fbf92122999
Nojotel <nojotel@yandex.ru>
2023-10-20 15:32:47 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,18 +49,23 @@ class VoiceRecorder {
 
   onMediaRecorderStop(e) {
     const blob = new Blob(this.chunks, { type: "audio/wav; codecs=opus" });
-
-    let req = new Request("https://farm.pythonanywhere.com/upload/", { method: "POST", body: blob, headers: { "Content-Type": "multipart/form-data" } });
+    /*const audioURL = window.URL.createObjectURL(blob);
+    this.playerRef.src = audioURL;
+    this.chunks = [];
+    this.stream.getAudioTracks().forEach((track) => track.stop());
+    this.stream = null;*/
+    let req = new Request("https://farm.pythonanywhere.com/upload/", {
+      method: "POST",
+      body: blob,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Cache-Control": "no-cache",
+      },
+    });
     fetch(req).then(
       () => console.log("Файл сохранен"),
       () => console.log("Ошибка сохранения")
     );
-    /* const audioURL = window.URL.createObjectURL(blob);
-    this.playerRef.src = audioURL;
-    this.chunks = [];
-    this.stream.getAudioTracks().forEach((track) => track.stop());
-    this.stream = null
-    ыы;*/
   }
 
   startRecording() {
